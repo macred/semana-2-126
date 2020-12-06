23851643d2f70c9ae1faa4856329a5efdda2a3be
andresgc1213 <andresgc1213@users.noreply.github.com>
2020-12-05 19:01:32 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+     <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      title : 'vivo en Vue'
+    }
+  }
+}
+</script>
+
---
+++ src/app.js
@@ -0,0 +1,31 @@
+var app = new Vue({
+
+    el : '#app',
+    data : {
+        teamUno : {
+            'nombre' :'Andres F. Giraldo',
+            'cargo' : 'Scrum Master',
+            'info' : 'Vivo en la cuidad de Cali. Apasionado de la electrónica y el desarrollo de software. Mis pasatiempos son la lectura, videojuegos y Anime.',
+
+        },
+        teamDos : {
+            'nombre' :'Yody Tovar',
+            'cargo' : 'Frontend',
+            'info' : 'Vivo en Fusagasugá, me dedico al comercio electrónico y me apasiona todo lo que se relacione con marketing digital, mis pasatiempos son la lectura y el deporte.',
+
+        },
+        teamTres : {
+            'nombre' :'Malory Camargo',
+            'cargo' : 'Backend',
+            'info' : 'Vivo en la ciudad de Bogotá, me interesa el desarrollo tecnológico aplicado a las humanidades digitales. Mis pasatiempos son la músicay los emprendimientos culturales.'
+
+        }
+
+    },
+    methods:{
+
+    },
+    computed :{
+
+    }
+})/*['Andres F. Giraldo','Yody Tovar','Malory Camargo']*/
